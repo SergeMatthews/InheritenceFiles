97470fa4b7b4368d893b73346ceb8fd34b29f0ac
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:30:39 +0600

    Update EquilateralTriangleClass.cpp

--- InheritenceFiles/EquilateralTriangleClass.cpp
+++ InheritenceFiles/EquilateralTriangleClass.cpp
@@ -1,5 +1,5 @@
 #include "EquilateralTriangleClass.h"
 
 EquilateralTriangle::EquilateralTriangle(int side_length) : IsoscelesTriangle(side_length, side_length, 60, 60) {
-    figureName = "Равносторонний треугольник";
+    figureName = "Р Р°РІРЅРѕСЃС‚РѕСЂРѕРЅРЅРёР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє";
 }
