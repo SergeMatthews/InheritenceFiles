d5e421a0b4be60c8f4c4f7afb683bc0dfee9c1f4
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:33:46 +0600

    Update RightTriangleClass.cpp

--- InheritenceFiles/RightTriangleClass.cpp
+++ InheritenceFiles/RightTriangleClass.cpp
@@ -3,5 +3,5 @@
 RightTriangle::RightTriangle(int length_a, int length_b, int length_c, int angle_A, int  angle_B) :
     Triangle(length_a, length_b, length_c, angle_A, angle_B, 90)
 {
-    figureName = "Прямоугольный треугольник";
+    figureName = "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє";
 }
