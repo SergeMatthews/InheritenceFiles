c9ce9911c993c44fa926f5d6b2e44608ac2dd923
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:32:25 +0600

    Update Parallelogram.cpp

--- InheritenceFiles/Parallelogram.cpp
+++ InheritenceFiles/Parallelogram.cpp
@@ -3,5 +3,5 @@
 Parallelogram::Parallelogram(int longSide_length, int shortSide_length, int acute_angle, int obtuse_angle) :
     Quadrangle(shortSide_length, longSide_length, shortSide_length, longSide_length,
         acute_angle, obtuse_angle, acute_angle, obtuse_angle) {
-    figureName = "Параллелограмм";
+    figureName = "РџР°СЂР°Р»Р»РµР»РѕРіСЂР°РјРј";
 }
