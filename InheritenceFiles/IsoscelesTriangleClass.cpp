0c132270188d32a4559a9bd08da826e4c3b78b77
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:32:06 +0600

    Update IsoscelesTriangleClass.cpp

--- InheritenceFiles/IsoscelesTriangleClass.cpp
+++ InheritenceFiles/IsoscelesTriangleClass.cpp
@@ -2,5 +2,5 @@
 
 IsoscelesTriangle::IsoscelesTriangle(int base_length, int sides_length, int base_angle, int top_angle) :
     Triangle(sides_length, base_length, sides_length, base_angle, top_angle, base_angle) {
-    figureName = "Равнобедренный треугольник";
+    figureName = "Р Р°РІРЅРѕР±РµРґСЂРµРЅРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє";
 }
