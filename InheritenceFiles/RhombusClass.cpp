bf062fa661ae4272900fe3922e863bb19067c13c
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:33:31 +0600

    Update RhombusClass.cpp

--- InheritenceFiles/RhombusClass.cpp
+++ InheritenceFiles/RhombusClass.cpp
@@ -2,5 +2,5 @@
 
 Rhombus::Rhombus(int side_length, int acute_angle, int obtuse_angle) :
     Parallelogram(side_length, side_length, acute_angle, obtuse_angle) {
-    figureName = "Ромб";
+    figureName = "Р РѕРјР±";
 }
