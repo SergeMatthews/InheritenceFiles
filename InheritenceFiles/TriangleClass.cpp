266c9e7a3cfa5a25a2a0d3ffbee39bb3136fc1ed
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-16 22:34:11 +0600

    Update TriangleClass.cpp

--- InheritenceFiles/TriangleClass.cpp
+++ InheritenceFiles/TriangleClass.cpp
@@ -1,7 +1,7 @@
 #include "TriangleClass.h"
 
 Triangle::Triangle(int length_a, int length_b, int length_c, int angle_A, int  angle_B, int angle_C) 
-    : Figure("Треугольник", 3),
+    : Figure("РўСЂРµСѓРіРѕР»СЊРЅРёРє", 3),
     length_a(length_a), length_b(length_b), length_c(length_c),
     angle_A(angle_A), angle_B(angle_B), angle_C(angle_C)
     {}
